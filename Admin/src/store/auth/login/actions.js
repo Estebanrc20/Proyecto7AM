fff81fdd583f39cfdb470dd69a682610548a43b4
Estebanrc20 <esteban.rc0120@gmail.com>
2025-06-05 16:59:33 -0500

    modificacion carpeta admin

--- Admin/src/store/auth/login/actions.js
+++ Admin/src/store/auth/login/actions.js
@@ -6,49 +6,12 @@ import {
   API_ERROR,
 } from "./actionTypes"
 
-import {
-  LOGIN_USER,
-  LOGIN_SUCCESS,
-  LOGOUT_USER,
-  LOGOUT_USER_SUCCESS,
-  API_ERROR,
-} from "./actionTypes"
-
-export const loginUser = (user, history) => {
+/*export const loginUser = (user, history) => {
   return {
     type: LOGIN_USER,
     payload: { user, history },
   }
-}
-
-export const loginSuccess = user => {
-  return {
-    type: LOGIN_SUCCESS,
-    payload: user,
-  }
-}
-
-export const logoutUser = history => {
-  return {
-    type: LOGOUT_USER,
-    payload: { history },
-  }
-}
-
-export const logoutUserSuccess = () => {
-  return {
-    type: LOGOUT_USER_SUCCESS,
-    payload: {},
-  }
-}
-
-export const apiError = error => {
-  return {
-    type: API_ERROR,
-    payload: error,
-  }
-}
-/*
+}]*/
 export const loginUser = (userData, navigate) => {
   return async (dispatch) => {
     try {
@@ -107,4 +70,4 @@ export const apiError = error => {
     type: API_ERROR,
     payload: error,
   }
-}*/
+}
