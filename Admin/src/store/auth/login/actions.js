17d2997bd2f01f056a0aae2979e9c0d80232464a
Estebanrc20 <esteban.rc0120@gmail.com>
2025-06-05 13:39:46 -0500

    modificacion en actions.js

--- Admin/src/store/auth/login/actions.js
+++ Admin/src/store/auth/login/actions.js
@@ -6,12 +6,49 @@ import {
   API_ERROR,
 } from "./actionTypes"
 
-/*export const loginUser = (user, history) => {
+import {
+  LOGIN_USER,
+  LOGIN_SUCCESS,
+  LOGOUT_USER,
+  LOGOUT_USER_SUCCESS,
+  API_ERROR,
+} from "./actionTypes"
+
+export const loginUser = (user, history) => {
   return {
     type: LOGIN_USER,
     payload: { user, history },
   }
-}]*/
+}
+
+export const loginSuccess = user => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: user,
+  }
+}
+
+export const logoutUser = history => {
+  return {
+    type: LOGOUT_USER,
+    payload: { history },
+  }
+}
+
+export const logoutUserSuccess = () => {
+  return {
+    type: LOGOUT_USER_SUCCESS,
+    payload: {},
+  }
+}
+
+export const apiError = error => {
+  return {
+    type: API_ERROR,
+    payload: error,
+  }
+}
+/*
 export const loginUser = (userData, navigate) => {
   return async (dispatch) => {
     try {
@@ -70,4 +107,4 @@ export const apiError = error => {
     type: API_ERROR,
     payload: error,
   }
-}
+}*/
