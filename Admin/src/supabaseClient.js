f6756e332a135d64feffd0eee48567ab5c32ce9e
Estebanrc20 <esteban.rc0120@gmail.com>
2025-06-17 16:36:53 -0500

    se modifica supabaseClient

--- Admin/src/supabaseClient.js
+++ Admin/src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://aborinpqezfrhguyzdxx.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFib3JpbnBxZXpmcmhndXl6ZHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTA5NTEsImV4cCI6MjA2NTMyNjk1MX0.DzQkceEqySnUA-TLuC40l2tZBLuH1qnw8eQkRDwl3lA'
+const supabaseUrl = 'https://mjcicgaubbtovahtdkvc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qY2ljZ2F1YmJ0b3ZhaHRka3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTIzMTcsImV4cCI6MjA2NTc2ODMxN30.RIOWNKiARi0j1RqDRG2wsBA1jucHETHUjhROiI1uqT0'
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
