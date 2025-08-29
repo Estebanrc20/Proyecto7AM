65f959cd7dff63805eaa6904157b4d778f8bb486
Estebanrc20 <esteban.rc0120@gmail.com>
2025-08-29 12:51:19 -0500

    Remove .env y modifica .env y supabaseclient

--- Admin/src/supabaseClient.js
+++ Admin/src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mjcicgaubbtovahtdkvc.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qY2ljZ2F1YmJ0b3ZhaHRka3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTIzMTcsImV4cCI6MjA2NTc2ODMxN30.RIOWNKiARi0j1RqDRG2wsBA1jucHETHUjhROiI1uqT0'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
